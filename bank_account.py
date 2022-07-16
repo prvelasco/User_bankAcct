a2534a70d9a752684438ef25eec75c8b138906b4
Patrick Velasco <102713460+prvelasco@users.noreply.github.com>
2022-07-15 21:34:46 -0700

    Add files via upload

---
+++ bank_account.py
@@ -0,0 +1,61 @@
+class BankAccount:
+    accounts = []
+
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+    
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    
+    def withdraw(self, amount):
+        if(self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+    
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+    
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+    @classmethod
+    def print_all_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info()
+
+class User:
+
+    def __init__(self, name):
+        self.name = name
+        self.account = {
+            "checking" : BankAccount(.02,1000),
+            "savings" : BankAccount(.05,3000)
+        }
+        
+
+    def user_balance(self):
+        print(f"User: {self.name}, Checking Balance: {self.account['checking'].display_account_info()}")
+        print(f"User: {self.name}, Savings Balance: {self.account['savings'].display_account_info()}")
+        return self
+
+    def transfer_money(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.user_balance()
+        user.user_balance()
+        return self
+
+
+Patrick = User("Patrick")
+
+Patrick.account['checking'].deposit(100)
+Patrick.user_balance()
